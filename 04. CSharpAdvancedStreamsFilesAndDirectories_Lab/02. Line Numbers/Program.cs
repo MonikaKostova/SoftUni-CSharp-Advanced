e59772e8f0b8fa7939aa2d0f70e66ccd1d2a23c7
MonikaKostova <137040970+MonikaKostova@users.noreply.github.com>
2024-01-12 10:56:50 +0200

    adding

---
+++ 04. CSharpAdvancedStreamsFilesAndDirectories_Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string
+       outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int row = 1;
+
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine($"{row++}. {reader.ReadLine()}");
+                }
+            }
+        }
+    }
+}
