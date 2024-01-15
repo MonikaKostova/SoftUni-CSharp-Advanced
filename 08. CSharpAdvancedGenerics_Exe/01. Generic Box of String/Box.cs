aeb1d9ce8848857f5b979bfae04567aac2c0a9c8
MonikaKostova <137040970+MonikaKostova@users.noreply.github.com>
2024-01-15 14:53:44 +0200

    adding

---
+++ 08. CSharpAdvancedGenerics_Exe/01. Generic Box of String/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T boxValue;
+
+        public T BoxValue { get { return boxValue; } set { boxValue = value; } }
+
+        public Box(T entry)
+        {
+            boxValue = entry;
+        }
+
+        public override string ToString() =>
+        $"{this.BoxValue.GetType()}: {this.BoxValue}";
+    }
+}
---
+++ 08. CSharpAdvancedGenerics_Exe/03. Generic Swap Method String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace GenericSwapMethodString
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<string> items = new List<string>();
+            int numberOfItems = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                items.Add(Console.ReadLine());
+            }
+            string input = Console.ReadLine();
+            int firstIndex = input.Split().Select(x => int.Parse(x)).First();
+            int secondIndex = input.Split().Select(x => int.Parse(x)).Last();
+            List<string> swappedList = Swap(items, firstIndex, secondIndex);
+            foreach (var item in swappedList)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public static List<T> Swap<T>(List<T> items, int firstIndex, int secondIndex)
+        {
+            T temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+            return items;
+        }
+    }
+}
